1a448e920c09b205cbfea79972229f89306bef7e
Marius Topircean <mtopircean@yahoo.com>
2023-07-10 19:04:39 +0000

    Added user menu and formula linked for first menu option

--- run.py
+++ run.py
@@ -14,6 +14,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('EssentialOils')
 
+program_menu = ("1. Add a product to the database", "2. List oils database",
+                "3. Search a product in the database", "4. Print patient database", "5. Search patient file")
+
+
+def list_menu(menu_options):
+    print("Options Menu:")
+    for option in menu_options:
+        print(option)
+    select_option = input(
+        "What do you want to do? Add the number of your option without any other characters:")
+    return select_option
+
 
 class Oils:
     """
@@ -31,18 +43,19 @@ class Oils:
         return f"Oil name: {self.name}\nAilment: {self.ailment}\nPrice: {self.price}\nNeeds difuser: {self.application}\nScore: {self.score}"
 
 
-my_oil = Oils()
+def add_oil():
+    my_oil = Oils()
 
-name = input("Input the name of the oil: ")
-ailment = input(
-    "Input the ailments the oil addresses by using a , to separate them without a space: ")
-price = float(input("Input the price value: "))
-application = input("Does it need a difuser(Yes/No): ")
-score = price / 100 + (0 if application == "yes" else 1)
+    name = input("Input the name of the oil: ")
+    ailment = input(
+        "Input the ailments the oil addresses by using a , to separate them without a space: ")
+    price = float(input("Input the price value: "))
+    application = input("Does it need a difuser(Yes/No): ")
+    score = price / 100 + (0 if application == "yes" else 1)
 
-my_oil.eo_catalogue(name, ailment, price, application, score)
-
-print(my_oil)
+    my_oil.eo_catalogue(name, ailment, price, application, score)
+    print("You added {name} to the database")
+    print(my_oil)
 
 
 def update_oils_worksheet(data, worksheet):
@@ -57,6 +70,24 @@ def update_oils_worksheet(data, worksheet):
     worksheet_master.insert_row(oil_data, following_row)
 
     print(f"{worksheet} updated.")
+
+
+selected_option = list_menu(program_menu)
+print("The option you have selected:", selected_option)
+
+if selected_option == "1":
+    add_oil()
+elif selected_option == "2":
+    pass
+elif selected_option == "3":
+    pass
+elif selected_option == "4":
+    pass
+elif selected_option == "5":
+    pass
+else:
+    print("You haven`t selected any of the options available. Please try again one of the options listed above.")
+    return selected_option
 
 
 data = [my_oil.name, my_oil.ailment, my_oil.price,
