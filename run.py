ba647cf95db6863eb900eacc4d483e2555719929
Marius Topircean <mtopircean@yahoo.com>
2023-07-10 18:13:53 +0000

    Added function to update worksheet master with oil data

--- run.py
+++ run.py
@@ -7,12 +7,13 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('EssentialOils')
+
 
 class Oils:
     """
@@ -29,10 +30,12 @@ class Oils:
     def __str__(self):
         return f"Oil name: {self.name}\nAilment: {self.ailment}\nPrice: {self.price}\nNeeds difuser: {self.application}\nScore: {self.score}"
 
+
 my_oil = Oils()
 
 name = input("Input the name of the oil: ")
-ailment = input("Input the ailments the oil addresses by using a , to separate them without a space: ")
+ailment = input(
+    "Input the ailments the oil addresses by using a , to separate them without a space: ")
 price = float(input("Input the price value: "))
 application = input("Does it need a difuser(Yes/No): ")
 score = price / 100 + (0 if application == "yes" else 1)
@@ -40,3 +43,22 @@ score = price / 100 + (0 if application == "yes" else 1)
 my_oil.eo_catalogue(name, ailment, price, application, score)
 
 print(my_oil)
+
+
+def update_oils_worksheet(data, worksheet):
+    """
+    Update excel sheet tab named master.
+    """
+    print(f"Updating {worksheet}.")
+    worksheet_master = SHEET.worksheet(worksheet)
+    following_row = len(worksheet_master.get_all_values()) + 1
+
+    oil_data = [data[0], data[1], data[2], data[3], data[4]]
+    worksheet_master.insert_row(oil_data, following_row)
+
+    print(f"{worksheet} updated.")
+
+
+data = [my_oil.name, my_oil.ailment, my_oil.price,
+        my_oil.application, my_oil.score]
+update_oils_worksheet(data, "master")
