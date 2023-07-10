2d29eeac86eb2b42b3c7db2acf7f474077f4b548
Marius Topircean <mtopircean@yahoo.com>
2023-07-10 06:14:47 +0000

    Created class and functions to populate oils class

--- run.py
+++ run.py
@@ -14,8 +14,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('EssentialOils')
 
-scope = SHEET.worksheet('scope')
+class Oils:
+    """
+    Oils class
+    """
 
-data = scope.get_all_values()
+    def eo_catalogue(self, name, ailment, price, application, score):
+        self.name = name
+        self.ailment = ailment
+        self.price = price
+        self.application = application
+        self.score = score
 
-print(data)
+    def __str__(self):
+        return f"Oil name: {self.name}\nAilment: {self.ailment}\nPrice: {self.price}\nNeeds difuser: {self.application}\nScore: {self.score}"
+
+my_oil = Oils()
+
+name = input("Input the name of the oil: ")
+ailment = input("Input the ailments the oil addresses by using a , to separate them without a space: ")
+price = float(input("Input the price value: "))
+application = input("Does it need a difuser(Yes/No): ")
+score = price / 100 + (0 if application == "yes" else 1)
+
+my_oil.eo_catalogue(name, ailment, price, application, score)
+
+print(my_oil)
