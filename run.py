41576dcd5b81b1d064e86b1271d7c0b92bca29b9
Marius Topircean <mtopircean@yahoo.com>
2023-07-18 17:01:34 +0000

    Installed Tabulate

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 import colorama
+from tabulate import tabulate
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
